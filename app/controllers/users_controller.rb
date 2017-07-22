5f66334f9910dc6832d88d0c3b16cfa6b73d9dfa
DESKTOP-56N9A7J\akihito_mano <khtmn8@gmail.com>
2017-07-22 14:28:43 +0900

    add_haml

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
 def index
+  "Hello World"
 end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  root 'users#Index'
+  root 'users#index'
 end
