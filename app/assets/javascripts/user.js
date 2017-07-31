bc85c66cbc6dbdfe19a8c994ecf0e29aeb5ca44f
DESKTOP-56N9A7J\akihito_mano <khtmn8@gmail.com>
2017-07-31 18:25:43 +0900

    add_popup_window

---
+++ app/assets/javascripts/user.js
@@ -0,0 +1,8 @@
+$(function() {
+  $('.detail_item_value').on('click', function(e) {
+    $('#popup_text').show();
+  });
+  $('.button_pink_a').on('click', function(e) {
+    $('#popup_text').hide();
+  });
+});
