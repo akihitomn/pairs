75b87a0b6de9348df4dd42dc5250f65305846331
DESKTOP-56N9A7J\akihito_mano <khtmn8@gmail.com>
2017-08-21 17:37:22 +0900

    add_columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170809064434) do
+ActiveRecord::Schema.define(version: 20170821061351) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "created_at", null: false
@@ -76,8 +76,11 @@ ActiveRecord::Schema.define(version: 20170809064434) do
     t.string "language"
     t.text "introdution"
     t.string "name"
+    t.string "uid"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
   add_foreign_key "groups_users", "groups"
