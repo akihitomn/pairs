b443e174c838bf8195fd2de4c3106606a042a5c1
DESKTOP-56N9A7J\akihito_mano <khtmn8@gmail.com>
2017-08-23 12:09:39 +0900

    fix_controller_and_routes

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,4 +5,21 @@ class MessagesController < ApplicationController
     @message = Message.new
   end
 
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      respond_to do |format|
+        format.html
+        format.json
+      end
+    else
+      flash.now[:alert] = 'メッセージか画像を入力して下さい'
+      render :index
+    end
+  end
+
+  private
+  def message_params
+      params.require(:message)).permit(:message, :image).merge(user_id: current_user.id)
+  end
 end
